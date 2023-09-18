42d97b1c16251f6fa6496aadbc53bbf87b7b4901
¨agustin-n-alesso <agustin.alesso@mercadolibre.com>
2023-09-17 21:15:08 -0300

    test

--- src/app/items/page.jsx
+++ src/app/items/page.jsx
@@ -3,6 +3,7 @@ export default async function ItemsPage({searchParams}){
 	const respuesta = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${searchParams.q}&limit=25`);
 	const data = await respuesta.json();
 	
+	
 	return (
 		<></>
 	)
